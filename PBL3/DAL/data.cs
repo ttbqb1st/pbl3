75d28dca2e3db8ebfebb4dba3cc4ff8802829613
ttbqb1st <55175356+ttbqb1st@users.noreply.github.com>
2025-05-13 23:19:57 +0700

    Add files via upload

--- PBL3/DAL/data.cs
+++ PBL3/DAL/data.cs
@@ -6,22 +6,23 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PBL3.DAL
 {
     internal class data
     {
-        private DataClasses1DataContext db = new DataClasses1DataContext();
         private static data instance;
         public static data Instance
         {
             get { if (instance == null) instance = new data(); return data.instance; }
             private set { data.instance = value; }
         }
-        public List<Nguoi_dung> getAllNguoiDung()
+        public List<User> getAllNguoiDung()
         {
+            DataClasses1DataContext db = new DataClasses1DataContext();
             return (from i in db.NGUOIDUNGs
-                    select new Nguoi_dung()
+                    select new User()
                     {
                         user_ID = Convert.ToInt32(i.MaNguoiDung),
                         username = i.TenDangNhap,
@@ -29,5 +30,86 @@ namespace PBL3.DAL
                         role_ID = Convert.ToInt32(i.VaiTroID)
                     }).ToList();
         }
+        public List<Customer_view_info> getAllCustomer()
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            return (from i in db.KHACHHANGs
+                    select new Customer_view_info()
+                    {
+                        customer_ID = Convert.ToInt32(i.MaKhachHang),
+                        user_ID = Convert.ToInt32(i.MaNguoiDung),
+                        customer_name = i.HoTen,
+                        ID_card_number = i.CMND,
+                        phone_number = i.SoDienThoai
+                    }).ToList();
+        }
+        public string getCustomerName(int user_id)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            var query = (from i in db.KHACHHANGs 
+                         join j in db.NGUOIDUNGs on i.MaNguoiDung equals j.MaNguoiDung
+                         where j.MaNguoiDung == user_id
+                         select i.HoTen).FirstOrDefault();
+            return query;
+        }
+
+        public List<Room_view> getRoomView(string txt)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            var query=from i in db.PHONGs
+                      join j in db.KHACHSANs on i.MaKhachSan equals j.MaKhachSan
+                      where j.TenKhachSan.Contains(txt)
+                      select new Room_view()
+                      {     room_ID = Convert.ToInt32(i.MaPhong),
+                          room_name = i.TenPhong,
+                          hotel_name = j.TenKhachSan,
+                          image = i.HinhAnh,
+                          price = Convert.ToInt32(i.GiaTien),
+                          location = j.DiaChi
+                      };
+            return query.ToList();
+        }
+        public int insertNguoiDung(User u)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            NGUOIDUNG entity = new NGUOIDUNG() { TenDangNhap = u.username, MatKhau = u.password, VaiTroID = u.role_ID };
+            db.NGUOIDUNGs.InsertOnSubmit(entity);
+            db.SubmitChanges();
+            return entity.MaNguoiDung;
+        }
+        public void insertKhachHang(Customer_view_info c)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            KHACHHANG entity = new KHACHHANG() { MaNguoiDung = c.user_ID, HoTen = c.customer_name, CMND = c.ID_card_number, SoDienThoai = c.phone_number };
+            db.KHACHHANGs.InsertOnSubmit(entity);
+            db.SubmitChanges();
+        }
+        public Room getRoomByID(int id)
+        {   DataClasses1DataContext db = new DataClasses1DataContext();
+            var query = from i in db.PHONGs
+                        join j in db.KHACHSANs on i.MaKhachSan equals j.MaKhachSan
+                        join k in db.LOAIPHONGs on i.MaLoaiPhong equals k.MaLoaiPhong
+                        join l in db.TRANGTHAIPHONGs on i.MaTrangThai equals l.MaTrangThai
+                        where i.MaPhong == id
+                        
+                        select new Room()
+                        {
+                            room_ID = Convert.ToInt32(i.MaPhong),
+                            room_name = i.TenPhong,
+                            hotel_name = j.TenKhachSan,
+                           location = j.DiaChi,
+                            room_type = k.TenLoaiPhong,
+                            price = Convert.ToInt32(i.GiaTien),
+                            status = l.TenTrangThai,
+                            description = i.MoTa,
+                            avatarImage = i.HinhAnh,
+                            detailedImages = (from m in db.ANHPHONGs
+                                              where m.MaPhong == i.MaPhong
+                                              select m.DuongDan).ToList()
+                        };
+            return query.FirstOrDefault();
+
+        }
+        
     }
 }
---
+++ PBL3/Presentation/Home(order page).Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PBL3.Presentation
+{
+    partial class Home_order_page_
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(353, 82);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(494, 300);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Images(Click to zoom)";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(44, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(368, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(626, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Add to favorites";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 414);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "To";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 414);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "From";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(269, 412);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(112, 22);
+            this.dateTimePicker2.TabIndex = 8;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(93, 412);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(112, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 460);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 616);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Rating";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(46, 675);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Customer review";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(141, 714);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(573, 150);
+            this.dataGridView2.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(350, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Status:";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PBL3.Properties.Resources._checked;
+            this.pictureBox4.Location = new System.Drawing.Point(454, 46);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PBL3.Properties.Resources.star;
+            this.pictureBox3.Location = new System.Drawing.Point(142, 609);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PBL3.Properties.Resources.favourite;
+            this.pictureBox2.Location = new System.Drawing.Point(733, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(47, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(421, 417);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 16);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(139, 460);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 16);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "label11";
+            // 
+            // Home_order_page_
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Location = new System.Drawing.Point(272, 26);
+            this.Name = "Home_order_page_";
+            this.Size = new System.Drawing.Size(987, 851);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
